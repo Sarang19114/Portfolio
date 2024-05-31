fe1182fd6e4585976b090bb93984220394438762
Sarang19114 <149702721+Sarang19114@users.noreply.github.com>
2024-05-31 10:49:52 +0000

    Changes done to the template

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Sarang Rastogi",
+  title: "B.Tech Student & Web-Developer",
+  email: "rastogi.sarang2004@gmail.com",
+  gitHub: "https://github.com/Sarang19114",
+  instagram: "https://www.instagram.com/sarang_rs19/",
+  linkedIn: "https://www.linkedin.com/in/sarang-rastogi-498948249/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "https://x.com/RastogiSarang",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Amazon Clone",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Amazon clone using HTML and CSS, replicating the design and layout of the e-commerce platform.",
+    url: "https://github.com/Sarang19114/Amazon_Clone",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Netflix Clone",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Netflix clone using HTML and CSS, replicating the design and user interface of the streaming platform.",
+    url: "https://github.com/Sarang19114/Netflix_Clone",
   },
   {
-    title: "My Resume Site",
+    title: "Sign Language Recogniton",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Developing a sign language recognition project using machine learning techniques, specifically Random Forest",
+    url: "Will Upload Soon",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Research Papers",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Authored a Research Paper on Cloud Gaming and Authoring one on Sign Language Recognition",
+    url: "Will Upload Soon",
   },
 ];
 
